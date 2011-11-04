e7f68b9b02a93e99ad1f2b6118a6eac1c652f135
Dan Rosén <danr@student.chalmers.se>
2011-11-04 18:19:38 +0100

    Add example of mutual recursive let binding
    Partially works, but an unnecessary function argument is added.

---
+++ src/Language/HFOL/FromHaskell/test/MutualLet.hs
@@ -0,0 +1,14 @@
+
+data Nat = Succ Nat | Zero
+data Bool = True | False
+
+even n = let even (Succ x) = not (odd x)
+             even Zero     = True
+             odd (Succ x)  = not (even x)
+             odd Zero      = True
+         in even n
+  where
+             not True      = False
+             not False     = True
+
+          
