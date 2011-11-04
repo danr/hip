beb7828d5fbba4e609fe1ef5e212db560252de39
Dan Rosén <danr@student.chalmers.se>
2011-11-04 14:01:13 +0100

    Improve concatMaybe implementation

--- src/Language/HFOL/Util.hs
+++ src/Language/HFOL/Util.hs
@@ -1,7 +1,8 @@
-module Language.HFOL.Util where
+module Language.HFOL.Util (selections,withPrevious,concatMaybe) where
 
 import Data.Maybe
 import Data.List
+import Test.QuickCheck
 
 -- | Pair up a list with its previous and next elements
 --
@@ -20,5 +21,13 @@ withPrevious xs = zip xs (inits xs)
 -- | If any is nothing (unreachable branch etc), return nothing,
 --   otherwise return just the catMaybes.
 concatMaybe :: [Maybe [a]] -> Maybe [a]
-concatMaybe ms | any isNothing ms = Nothing
-               | otherwise        = Just (concat (catMaybes ms))
+concatMaybe = fmap concat . sequence
+
+-- | Alternative implementation
+concatMaybe' :: [Maybe [a]] -> Maybe [a]
+concatMaybe' ms | any isNothing ms = Nothing
+                | otherwise        = Just (concat (catMaybes ms))
+
+-- | Test equality of implementations
+prop_concats :: [Maybe [Bool]] -> Bool
+prop_concats ms = concatMaybe ms == concatMaybe' ms
