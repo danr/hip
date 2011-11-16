c64a2a51202a08149210ca19c969176435d2eb05
Dan Rosén <danr@student.chalmers.se>
2011-11-16 10:11:11 +0100

    Make so zero case degenerates into no induction

--- tptp-test/DepthInduction.hs
+++ tptp-test/DepthInduction.hs
@@ -30,31 +30,19 @@ decls =
   , axiom "plussucc" $ forall' $ \y   -> succ zero     + y === succ y
   , axiom "plusstep" $ forall' $ \x y -> succ (succ x) + y === succ (succ (x + y))
 
-
 {-
   , axiom "pluszero" $ forall' $ \y   -> zero   + y === y
   , axiom "plusstep" $ forall' $ \x y -> succ x + y === succ (x + y)
-  -}
+-}
 
-  , axiom "ihstart" $ forall' $ \x   -> p x <=> ih primZero x
+  , axiom "ihstart" $ forall' $ \x   -> ih primZero x
   , axiom "ihstep"  $ forall' $ \x k -> (ih k (succ x) & p x) <=> ih (primSucc k) x
 
-  , axiom "isstart" $ forall' $ \n'    -> p (succ n')    ==> is primZero n'
+  , axiom "isstart" $ forall' $ \n'    -> p n'           ==> is primZero n'
   , axiom "isstep"  $ forall' $ \n' k  -> is k (succ n') ==> is (primSucc k) n'
 
   , question "induction" $ exists' $ \k -> ih k zero & (ih k n ==> is k n)
 
---  , question "testbasecases" $ exists' $ \k -> ih k zero
-
-
-
-{-  , axiom "pz"  $ p zero
-  , axiom "psz" $ p (succ zero)
-  , axiom "psz" $ p (succ (succ zero))
-  , axiom "ih1" $ p n
-  , axiom "ih2" $ p (succ n)
-  , axiom "ind" $ p (succ (succ n))
-  -}
   ]
 
 main :: IO ()
