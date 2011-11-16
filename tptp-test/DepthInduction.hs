1e9741634c1775cd518fe430a4e14b249caff4f5
Dan Rosén <danr@student.chalmers.se>
2011-11-16 09:54:12 +0100

    Add working depth induction

--- tptp-test/DepthInduction.hs
+++ tptp-test/DepthInduction.hs
@@ -3,10 +3,11 @@ module Main where
 import Language.TPTP.Monad
 import Language.TPTP.Pretty
 
-import Prelude hiding (succ)
+import Prelude hiding (succ,(+))
 
 zero = constant "zero"
 succ = unary    "succ"
+(+)  = binary   "plus"
 
 n = constant "n"
 
@@ -19,28 +20,41 @@ p = predicate "p"
 
 ih = relation "ih"
 is = relation "is"
-ip = relation "ip"
 
 decls :: [Decl]
 decls =
-  [ axiom "ihstart" $ p zero ==> ih (primSucc primZero) zero
-  , axiom "ihstep"  $ forall' $ \m k -> ih k m & p (succ m)
-                                    ==> ih (primSucc k) (succ m)
+  [ axiom "p_r_id"   $ forall' $ \x   -> p x <=> (x + zero === x)
 
-  , axiom "isstart" $ forall' $ \n' -> is primZero n'
-  , axiom "isstep"  $ forall' $ \n' k -> is k (succ n') & p n' ==> is (primSucc k) n'
 
-  , axiom "ipstart" $ forall' $ \n' -> p n' ==> ip primZero n'
-  , axiom "ipstep"  $ forall' $ \n' k  -> ip k (succ n') ==> ip (primSucc k) n'
+  , axiom "pluszero" $ forall' $ \y   -> zero          + y === y
+  , axiom "plussucc" $ forall' $ \y   -> succ zero     + y === succ y
+  , axiom "plusstep" $ forall' $ \x y -> succ (succ x) + y === succ (succ (x + y))
 
-  , question "istest" $ exists' $ \k m -> ih (primSucc k) m & (is k n ==> ip k n)
 
-  , axiom "pz"  $ p zero
+{-
+  , axiom "pluszero" $ forall' $ \y   -> zero   + y === y
+  , axiom "plusstep" $ forall' $ \x y -> succ x + y === succ (x + y)
+  -}
+
+  , axiom "ihstart" $ forall' $ \x   -> p x <=> ih primZero x
+  , axiom "ihstep"  $ forall' $ \x k -> (ih k (succ x) & p x) <=> ih (primSucc k) x
+
+  , axiom "isstart" $ forall' $ \n'    -> p (succ n')    ==> is primZero n'
+  , axiom "isstep"  $ forall' $ \n' k  -> is k (succ n') ==> is (primSucc k) n'
+
+  , question "induction" $ exists' $ \k -> ih k zero & (ih k n ==> is k n)
+
+--  , question "testbasecases" $ exists' $ \k -> ih k zero
+
+
+
+{-  , axiom "pz"  $ p zero
   , axiom "psz" $ p (succ zero)
   , axiom "psz" $ p (succ (succ zero))
   , axiom "ih1" $ p n
   , axiom "ih2" $ p (succ n)
   , axiom "ind" $ p (succ (succ n))
+  -}
   ]
 
 main :: IO ()
