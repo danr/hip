f874f5da459ab376d5160a63a34c2032c3b777b9
Dan Rosén <danr@student.chalmers.se>
2011-11-16 09:25:05 +0100

    Add depth induction that seems to work for nats

--- tptp-test/DepthInduction.hs
+++ tptp-test/DepthInduction.hs
@@ -13,26 +13,34 @@ n = constant "n"
 primZero = constant "primzero"
 primSucc = unary    "primsucc"
 
+primNat = predicate "primnat"
+
 p = predicate "p"
 
 ih = relation "ih"
 is = relation "is"
+ip = relation "ip"
 
 decls :: [Decl]
 decls =
-  [ axiom "ihstart" $ p zero ==> ih primZero zero
+  [ axiom "ihstart" $ p zero ==> ih (primSucc primZero) zero
   , axiom "ihstep"  $ forall' $ \m k -> ih k m & p (succ m)
                                     ==> ih (primSucc k) (succ m)
-  , axiom "isstart" $ forall' $ \n' -> (p n' ==> p (succ n'))
-                                   ==> is primZero n'
-  , axiom "isstep"  $ forall' $ \n' k -> (p n' ==> is k (succ n'))
-                                     ==> is (primSucc k) n'
-  , question "ind"  $ exists' $ \k m -> ih k m & is k n
 
-  , axiom "pz" $ p zero
+  , axiom "isstart" $ forall' $ \n' -> is primZero n'
+  , axiom "isstep"  $ forall' $ \n' k -> is k (succ n') & p n' ==> is (primSucc k) n'
+
+  , axiom "ipstart" $ forall' $ \n' -> p n' ==> ip primZero n'
+  , axiom "ipstep"  $ forall' $ \n' k  -> ip k (succ n') ==> ip (primSucc k) n'
+
+  , question "istest" $ exists' $ \k m -> ih (primSucc k) m & (is k n ==> ip k n)
+
+  , axiom "pz"  $ p zero
   , axiom "psz" $ p (succ zero)
---  , axiom "psz" $ p (succ (succ zero))
-  , axiom "ind" $ forall' $ \m -> p m ==> p (succ (succ (succ m)))
+  , axiom "psz" $ p (succ (succ zero))
+  , axiom "ih1" $ p n
+  , axiom "ih2" $ p (succ n)
+  , axiom "ind" $ p (succ (succ n))
   ]
 
 main :: IO ()
