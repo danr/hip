2543ded2adb0b8b7260901fc985a548066ee251b
Dan Rosén <danr@student.chalmers.se>
2012-02-05 01:13:56 +0100

    Update future work, move fastloosereasoning to related

--- src/Autospec/Params.hs
+++ src/Autospec/Params.hs
@@ -49,21 +49,21 @@ defParams = Params
   , output      = Nothing &= opt  "proving/" &= typDir &= help "Output all tptp files in a directory (default proving/)"
   , statistics  = False   &= help "Generate statistics files (run with --reprove)"
 
-  , processes   = 4       &= help "Number of prover processes (default 4)" &= name "p" &= groupname "Proving settings"
+  , processes   = 4       &= help "Number of prover processes (default 4)" &= name "p" &= groupname "\nProving settings"
   , timeout     = 1       &= help "Timout of provers in seconds (default 1)" &= name "t"
   , provers     = "evpsx" &= help "Provers to use (e)prover (v)ampire (p)rover9 (s)pass equino(x)"
   , methods     = "pisfa" &= help "Methods to use (p)lain (i)nduction (s)tructural (f)ixpoint (a)pprox"
   , reprove     = False   &= help "Reprove theorems already known to be true"
   , consistency = False   &= help "Try to prove the consistency a file's generated theory"
 
-  , inddepth    = 1       &= help "Max depth to do structural induction (default 1)" &= groupname "Structural induction"
+  , inddepth    = 1       &= help "Max depth to do structural induction (default 1)" &= groupname "\nStructural induction"
   , indvars     = 1       &= help "Number of variables to do structural induction on (default 1)"
   , indhyps     = 200     &= help "Maximum number of hypotheses from structural induction (default 200)"
   , indsteps    = 10      &= help "Maximum number of step cases from structural induction (default 10)"
 
-  , fpimax      = 25      &= help "Maximum number of lifted functions (default 25)" &= groupname "Fixpoint induction"
+  , fpimax      = 25      &= help "Maximum number of lifted functions (default 25)" &= groupname "\nFixpoint induction"
 
-  , tptp        = False   &= help "Generate tptp output and terminate" &= name "f"  &= groupname "Debug"
+  , tptp        = False   &= help "Generate tptp output and terminate" &= name "f"  &= groupname "\nDebug"
   , core        = False   &= help "Translate hs to core and terminate" &= name "c"
   , dbfh        = False   &= help "Print debug output of Haskell -> Core translation"
   , dbfol       = False   &= help "Print debug output of Core -> FOL"
--- testsuite/PatternMatching.hs
+++ testsuite/PatternMatching.hs
@@ -12,6 +12,8 @@ or2 a     b = b
 or3 False False = False
 or3 a     b     = True
 
+{-
+
 prop_or12eq :: Prop (Bool -> Bool -> Bool)
 prop_or12eq = or1 =:= or2
 
@@ -20,6 +22,8 @@ prop_or23eq = or2 =:= or3
 
 prop_or13eq :: Prop (Bool -> Bool -> Bool)
 prop_or13eq = or1 =:= or3
+
+-}
 
 data Tree = Leaf | Bin Tree Tree
   deriving (Eq,Show)
@@ -31,6 +35,27 @@ instance Arbitrary Tree where
                                    b <- arb (s `div` 2)
                                    return (Bin a b)
                              )]
+
+mirror2fix (Bin a b) = Bin left right
+  where
+    left = case b of
+      Bin l r -> Bin (mirror2fix r) (mirror2fix l)
+      Leaf    -> Leaf
+    right = case a of
+      Bin l r -> Bin (mirror2fix r) (mirror2fix l)
+      Leaf    -> Leaf
+mirror2fix Leaf = Leaf
+
+mirror2wfix (Bin a b) = Bin left right
+  where
+    left = case b of
+      Bin l r -> Bin (mirror2wfix r) (mirror2wfix l)
+      _       -> Leaf
+    right = case a of
+      Bin l r -> Bin (mirror2wfix r) (mirror2wfix l)
+      _       -> Leaf
+mirror2wfix _ = Leaf
+
 
 mirror2 (Bin a b) =
   case a of
@@ -58,6 +83,7 @@ mirror Leaf       = Leaf
 mirrorw (Bin a b) = Bin (mirrorw b) (mirrorw a)
 mirrorw n       = n
 
+{-
 
 prop_tests :: Prop Tree
 prop_tests = mirror2 (Bin (Bin undefined undefined) Leaf) =:= Bin Leaf (Bin undefined undefined)
@@ -70,6 +96,16 @@ prop_testp = mirror (Bin (Bin undefined undefined) Leaf) =:= Bin Leaf (Bin undef
 
 prop_testw :: Prop Tree
 prop_testw = mirrorw (Bin (Bin undefined undefined) Leaf) =:= Bin Leaf (Bin undefined undefined)
+
+-}
+
+prop_mirror2fix :: Tree -> Prop Tree
+prop_mirror2fix x = mirror2fix (mirror2fix x) =:= x
+
+prop_mirror2wfix :: Tree -> Prop Tree
+prop_mirror2wfix x = mirror2wfix (mirror2wfix x) =:= x
+
+{-
 
 prop_mirror2 :: Tree -> Prop Tree
 prop_mirror2 x = mirror2 (mirror2 x) =:= x
@@ -94,6 +130,23 @@ prop_eq_2w_w = mirror2w =:= mirrorw
 
 prop_eq_2w_2 :: Prop (Tree -> Tree)
 prop_eq_2w_2 = mirror2w =:= mirror2
+
+prop_eq_2wfix_2fix :: Prop (Tree -> Tree)
+prop_eq_2wfix_2fix = mirror2wfix =:= mirror2fix
+
+prop_eq_2wfix_ :: Prop (Tree -> Tree)
+prop_eq_2wfix_ = mirror2wfix =:= mirror
+
+prop_eq_2fix_w :: Prop (Tree -> Tree)
+prop_eq_2fix_w = mirror2wfix =:= mirrorw
+
+prop_eq_2wfix_w :: Prop (Tree -> Tree)
+prop_eq_2wfix_w = mirror2wfix =:= mirrorw
+
+prop_eq_2fix_ :: Prop (Tree -> Tree)
+prop_eq_2fix_ = mirror2 =:= mirror
+
+-}
 
 main = do
   quickCheck (printTestCase "prop_or12eq"   (prop_or12eq :: Prop (Bool -> Bool -> Bool)))
