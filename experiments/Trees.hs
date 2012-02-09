63e7c4f696e0da540d3418f734fbf223476383ef
Dan Rosén <danr@student.chalmers.se>
2012-02-09 21:19:49 +0100

    Add final presentation

--- experiments/Trees.hs
+++ experiments/Trees.hs
@@ -12,13 +12,13 @@ return = Leaf
 Fork u v >>= f = Fork (u >>= f) (v >>= f)
 Leaf x   >>= f = f x
 
+prop_return_right t  = t >>= return     =:=  t
+
+prop_return_left f x = return x >>= f   =:=  f x
+
+prop_assoc t f g     = (t >>= f) >>= g  =:=  t >>= (\x -> f x >>= g)
+
 prop_return_right :: Tree a -> Prop (Tree a)
-prop_return_right t = t >>= return =:= t
-
 prop_return_left :: (a -> Tree b) -> a -> Prop (Tree b)
-prop_return_left f x = return x >>= f =:= f x
-
 prop_assoc :: Tree a -> (a -> Tree b) -> (b -> Tree c) -> Prop (Tree c)
-prop_assoc t f g = (t >>= f) >>= g =:= t >>= (\x -> f x >>= g)
-
 
